1e2ca7b489de166128dc513921b6ffda6de4ec73
YookyungJung <yugyungs200@naver.com>
2017-12-19 09:29:52 +0900

    Network 기본적인 예제(InetAdress클래스)

---
+++ Network/src/InetAddress/InetAddressExam.java
@@ -0,0 +1,14 @@
+package InetAddress;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class InetAddressExam {
+
+	public static void main(String[] args) throws UnknownHostException {
+		InetAddress ia=InetAddress.getLocalHost();
+		System.out.println("IP¡÷º“: "+ia.getHostAddress());
+
+	}
+
+}
---
+++ Network/src/InetAddress/InetAddressExam2.java
@@ -0,0 +1,14 @@
+package InetAddress;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class InetAddressExam2 {
+	public static void main(String[] args) {
+		try {
+			InetAddress ia = InetAddress.getByName("java.sun.com");
+			System.out.println("IP주소: "+ia.getHostAddress());
+			System.out.println("도메인 이름: "+ia.getHostName());
+		} catch (UnknownHostException e) { e.printStackTrace(); }
+	}
+}
